014675d4ae231d33139a9f9d21b26c2b70012976
Henrik Knutsen <hknots99@gmail.com>
2022-09-02 11:41:43 +0200

    Refactor (#2)

---
+++ src/main/java/no/fint/FintMimeMessage.java
@@ -0,0 +1,47 @@
+package no.fint;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class FintMimeMessage {
+
+    private final MimeMessage mimeMessage;
+
+    public FintMimeMessage(Config config, String content, String subject) throws MessagingException {
+        mimeMessage = new MimeMessage(createSession(config));
+
+        mimeMessage.setFrom(new InternetAddress(config.getSmtpUsername()));
+
+        for (String recipient : config.getRecipients()) { addRecipient(recipient); }
+        mimeMessage.setSubject(subject);
+        mimeMessage.setText(content, "UTF-8", "html");
+    }
+
+    private void addRecipient(String recipient) throws MessagingException {
+        mimeMessage.addRecipient(Message.RecipientType.TO,new InternetAddress(recipient));
+    }
+
+    private Session createSession(Config config) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", config.getSmtpHost());
+        props.put("mail.smtp.port", config.getSmtpPort());
+
+        return Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(config.getSmtpUsername(), config.getSmtpPassword());
+                    }
+                });
+    }
+
+    public MimeMessage getMail() {
+        return mimeMessage;
+    }
+}
