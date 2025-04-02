5b6c4899588c2c31f96dd66b4ba4daee4ecb3e13
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-04-02 08:57:16 +0200

    TEST: fix test

--- src/test/groovy/no/fint/TemplateSpec.groovy
+++ src/test/groovy/no/fint/TemplateSpec.groovy
@@ -3,6 +3,7 @@ package no.fint
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.fint.model.administrasjon.organisasjon.Organisasjonselement
 import no.fint.model.felles.kompleksedatatyper.Identifikator
+import no.fint.model.felles.kompleksedatatyper.Periode
 import no.fint.organization.SimpleOrganizationInfo
 import no.fint.utils.TemplateService
 import org.jooq.lambda.tuple.Tuple2
@@ -19,55 +20,66 @@ class TemplateSpec extends Specification{
     @Autowired
     TemplateService templateService
 
-    //TODO: fix and activate test
-//    def 'Template renders properly'() {
-//        given:
-//        def added = [
-//                new OrganizationDocument(
-//                        data: new Organisasjonselement(
-//                                navn: 'En liten test',
-//                                kortnavn: 'test',
-//                                organisasjonsKode: new Identifikator(identifikatorverdi: '123'),
-//                                organisasjonsId: new Identifikator(identifikatorverdi: '2')
-//                        )
-//                )
-//        ]
-//        def updated = [
-//                Tuple2.tuple(
-//                        new OrganizationDocument(
-//                                data: new Organisasjonselement(
-//                                        navn: 'Gammelt navn',
-//                                        kortnavn: 'gammel',
-//                                        organisasjonsKode: new Identifikator(identifikatorverdi: '123'),
-//                                        organisasjonsId: new Identifikator(identifikatorverdi: '2')
-//                                )
-//                        ),
-//                        new OrganizationDocument(
-//                                data: new Organisasjonselement(
-//                                        navn: 'Nytt navn',
-//                                        kortnavn: 'ny',
-//                                        organisasjonsKode: new Identifikator(identifikatorverdi: '123'),
-//                                        organisasjonsId: new Identifikator(identifikatorverdi: '2')
-//                                )
-//                        )
-//                )
-//        ]
-//        def parentInfo = [
-//                new SimpleOrganizationInfo("1", "Østfold fylkeskommune")
-//                ]
-//
-//        when:
-//        String result = templateService.render(added, updated, parentInfo)
-//        println(result)
-//
-//        then:
-//        result.contains('er nye:')
-//        result.contains('er endret:')
-//        result.contains('En liten test')
-//        result.contains('Gammelt navn')
-//        result.contains('Nytt navn')
-//        result.contains('Overordnede organisasjonsenheter:')
-//    }
+    def 'Template renders properly'() {
+        given:
+        def added = [
+                new OrganizationDocument(
+                        data: new Organisasjonselement(
+                                navn: 'En liten test',
+                                kortnavn: 'test',
+                                organisasjonsKode: new Identifikator(identifikatorverdi: '123'),
+                                organisasjonsId: new Identifikator(identifikatorverdi: '2'),
+                                gyldighetsperiode: new Periode(){{
+                                    setStart(new Date())
+                                    setSlutt(new Date())
+                                }}
+                        )
+                )
+        ]
+        def updated = [
+                Tuple2.tuple(
+                        new OrganizationDocument(
+                                data: new Organisasjonselement(
+                                        navn: 'Gammelt navn',
+                                        kortnavn: 'gammel',
+                                        organisasjonsKode: new Identifikator(identifikatorverdi: '123'),
+                                        organisasjonsId: new Identifikator(identifikatorverdi: '2'),
+                                        gyldighetsperiode: new Periode(){{
+                                            setStart(new Date())
+                                            setSlutt(new Date())
+                                        }}
+                                )
+                        ),
+                        new OrganizationDocument(
+                                data: new Organisasjonselement(
+                                        navn: 'Nytt navn',
+                                        kortnavn: 'ny',
+                                        organisasjonsKode: new Identifikator(identifikatorverdi: '123'),
+                                        organisasjonsId: new Identifikator(identifikatorverdi: '2'),
+                                        gyldighetsperiode: new Periode(){{
+                                            setStart(new Date())
+                                            setSlutt(new Date())
+                                        }}
+                                )
+                        )
+                )
+        ]
+        def parentInfo = [
+                new SimpleOrganizationInfo("1", "Østfold fylkeskommune")
+                ]
+
+        when:
+        String result = templateService.render(added, updated, parentInfo)
+        println(result)
+
+        then:
+        result.contains('er nye:')
+        result.contains('er endret:')
+        result.contains('En liten test')
+        result.contains('Gammelt navn')
+        result.contains('Nytt navn')
+        result.contains('Overordnede organisasjonsenheter:')
+    }
 
     def 'Template renders with empty inputs'() {
         when:
