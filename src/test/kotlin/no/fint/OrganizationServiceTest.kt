dd2c8db01777acb784a6bf6b465d19c09459e83d
daniejoh <github@danieljohnsen.no>
2026-01-09 15:06:01 +0100

    test: add more wiremock responses

--- src/test/kotlin/no/fint/OrganizationServiceTest.kt
+++ src/test/kotlin/no/fint/OrganizationServiceTest.kt
@@ -1,5 +1,8 @@
 package no.fint
 
+import no.fint.model.administrasjon.organisasjon.Organisasjonselement
+import no.fint.model.felles.kompleksedatatyper.Identifikator
+import no.fint.model.felles.kompleksedatatyper.Periode
 import no.fint.organization.OrganizationDocument
 import no.fint.organization.OrganizationRepository
 import no.fint.organization.OrganizationService
@@ -19,26 +22,50 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.wiremock.spring.ConfigureWireMock
 import org.wiremock.spring.EnableWireMock
+import java.util.Date
 
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("test")
+@Transactional // To reset database before each test.
 class OrganizationServiceTest(
     @Autowired private val organizationRepository: OrganizationRepository,
     @Autowired private val organizationService: OrganizationService,
 ) : BaseIntegrationTest() {
     @BeforeEach
     fun setUp() {
+        val organisasjonselement =
+            Organisasjonselement().apply {
+                organisasjonsId = Identifikator().apply { identifikatorverdi = "762" }
+                organisasjonsKode = Identifikator().apply { identifikatorverdi = "V40.44.17" }
+                organisasjonsnummer = Identifikator().apply { identifikatorverdi = "974544520" }
+                navn = "VGKALN Kalnes videregående skole"
+                kortnavn = "VGKALN"
+                gyldighetsperiode =
+                    Periode().apply {
+                        start = Date.from(java.time.Instant.parse("2019-04-01T00:00:00Z"))
+                    }
+            }
+
         organizationRepository.save(
             OrganizationDocument(
-                id = "123",
+                id = "762",
                 orgId = "viken.no",
-                data = null,
+                data = organisasjonselement,
+                overordnet = "https://api.felleskomponent.no/administrasjon/organisasjon/organisasjonselement/organisasjonsId/156",
+                underordnet =
+                    listOf(
+                        "https://api.felleskomponent.no/administrasjon/organisasjon/organisasjonselement/organisasjonsId/763",
+                        "https://api.felleskomponent.no/administrasjon/organisasjon/organisasjonselement/organisasjonsId/766",
+                        "https://api.felleskomponent.no/administrasjon/organisasjon/organisasjonselement/organisasjonsId/770",
+                        "https://api.felleskomponent.no/administrasjon/organisasjon/organisasjonselement/organisasjonsId/775",
+                    ), // this is missing one underordnet compared to what wiremock returns
             ),
         )
     }
 
     @Test
+    @Transactional
     fun `should not do anything when there is no update`() {
         organizationService.update()
         // assert nothing has changed in database
